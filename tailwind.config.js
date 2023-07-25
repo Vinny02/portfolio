21e20d1478d97266af4e58374e8ed0d86ceee481
Vinny02 <vinny.baijoo@gmail.com>
2023-07-24 18:08:46 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+
+    colors: {
+      'black': '#000000',
+      'white': '#FFFFFF',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
